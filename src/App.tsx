86d31bc41fa8056073ae3e95a0c29d33f19c1bf3
todd7 <edson_a@outlook.com>
2020-07-19 00:07:26 -0300

    removed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, DefaultTheme } from 'styled-components';
 
 import usePersistedState from './utils/usePersistedState';
@@ -8,12 +7,10 @@ import light from './styles/themes/light';
 import dark from './styles/themes/dark';
 
 import GlobalStyle from './styles/global';
-// import Routes from './routes';
 
 import Home from './pages/Home';
 
 const App: React.FC = () => {
-  // const [theme, setTheme] = useState(light);
   const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
 
   const toggleTheme = () => {
@@ -22,10 +19,6 @@ const App: React.FC = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <BrowserRouter>
-        <Routes />
-      </BrowserRouter> */}
-
       <Home toggleTheme={toggleTheme} />
 
       <GlobalStyle />
