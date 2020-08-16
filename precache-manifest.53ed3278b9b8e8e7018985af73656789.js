6162c62ea9c32e591f550c4e92ec5d59c15df313
todd7 <edson_a@outlook.com>
2020-08-16 18:36:38 -0300

    Updates

---
+++ precache-manifest.53ed3278b9b8e8e7018985af73656789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2676d70ac9eec8a7fd47c4c3fea8a2a",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "4264219e670451e7fb1c",
+    "url": "/my-resume/static/js/2.4035fcc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-resume/static/js/2.4035fcc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6a5d616076f9a03be9",
+    "url": "/my-resume/static/js/main.7017d158.chunk.js"
+  },
+  {
+    "revision": "37aab9b9e92e743b16a3",
+    "url": "/my-resume/static/js/runtime-main.379129a5.js"
+  },
+  {
+    "revision": "dce356b7e4cb20d3d5ea39eeabbade7b",
+    "url": "/my-resume/static/media/profile.dce356b7.jpg"
+  }
+]);
